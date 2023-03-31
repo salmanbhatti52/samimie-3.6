a68395b656668e5070671fc2eae26f5d732529b7
Salman Ahmad Bhatti <salmanbhatti52@hotmail.com>
2023-03-31 12:45:19 +0500

    initial code

---
+++ build/ios/Debug-iphonesimulator/social_share/social_share.framework/Headers/social_share-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "SocialSharePlugin.h"
+
+FOUNDATION_EXPORT double social_shareVersionNumber;
+FOUNDATION_EXPORT const unsigned char social_shareVersionString[];
+
